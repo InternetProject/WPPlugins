f3c66070626a054f497cba446b5ad0a044997005
RobMGibson <robert.gibson.2017@anderson.ucla.edu>
2017-02-06 19:06:14 +0000

    Add files via upload

---
+++ ip-club-events/Internet Project Club Events.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Plugin Name: Internet Project Club Events Info
+ * Description: A plugin for retrieving information about events of an specific Anderson Club
+ * Version: 1.1
+ * Author: Gustavo Panez
+ * License: GPLv2
+ *
+ * Copyright 2017  Internet Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+function ip_club_events($post_object) {
+  if (is_page('events')) {
+    $html .= '<div class="wrap">';
+    $html .= '<div id="ip-club-events-ajax-list"><h2>Upcoming Club Events Info</h2></div>';
+    echo $html;
+  ?>
+    <script type="text/javascript" >
+    jQuery(document).ready(function($) {
+		var clubId = 1;
+		var url = "http://35.164.255.95:8081/api/ClubsWS/" + clubId + "/Events";		
+		//var url = "http://thing120170201021741.azurewebsites.net/api/ClubsWS/" + clubId + "/Events";
+		//var url = "http://localhost:5050/api/ClubsWS/" + clubId + "/Events";
+        $.ajax({
+		  method: "GET",
+  	      url: url,
+	      contentType: 'application/json'
+        })
+        .done(function( data ) {
+          console.log('Successful AJAX Call! /// Return Data: ' + data);
+		  
+		  for (var i = 0; i < data.length; i++) {
+			var item = data[i];
+			var openTo;
+			if (item.isPublic) {
+				console.log('check public: ' + item.isPublic);
+				openTo = 'Everyone';
+			}
+			else {
+				openTo = item.clubs;
+			}
+			var html = 
+				'<div class="row"> \
+					<div class="col"> \
+						<div class="panel panel-primary"> \
+							<div class="panel-heading"> \
+								<h3 class="panel-title">' + item.title + '</h3> \
+							</div> \
+							<div class="panel-body"> \
+								<dl class="list-unlisted"> \
+									<dt>Date: ' + item.date + '</dt>\
+									<dt>Time: ' + item.startsAt + ' to ' + item.endsAt + '</dt>\
+									<dt>Location: ' + item.location + '</dt> \
+									<dt>Description: ' + item.description + '</dt> \
+									<dt>Open to: ' + openTo + '</dt> \
+									<dt>Food: ' + item.food + '</dt> \
+									<dt>Contact: ' + item.contact + '</dt> \
+									<dt>Price: ' + item.price + '</dt> \
+								</dl> \
+							</div> \
+						</div> \
+					</div> \
+				</div>';
+			$('#ip-club-events-ajax-list').append(html);
+		  }
+        })
+        .fail(function( data ) {
+          console.log('Failed AJAX Call :( /// Return Data: ' + data);
+        });
+    });
+    </script>
+  <?php
+  }
+}
+add_action('the_post', 'ip_club_events');
+?>
